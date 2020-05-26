2e26ff7b87b38b9ac81affdbe4c540821d4a8b60
Jashaswimalya Acharjee <31704400+jash-maester@users.noreply.github.com>
2020-05-26 21:47:10 +0530

    fixed smooth scrolling & logos
    some logo required removal of backgrounds and others required a semi translucent background for fully visible texts

--- js/main.js
+++ js/main.js
@@ -10,9 +10,9 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'sk.eyJ1IjoiamFzaG1hZXN0ZXIiLCJhIjoiY2s0bXV5b3QzMG92ZjNtbWlncGNvaDJmZyJ9.uTsP83Vu5ap6JST-YsjNxQ'
 }).addTo(mymap);
 var marker = L.marker([23.8408767, 91.4214196]).addTo(mymap);
-marker.bindPopup('<b>NIT Agartala</b><br>SANGANAN PRAYOG<br> <a target="_blank" href="https://www.google.com/maps/place/National+Institute+of+Technology+Agartala/@23.8406804,91.4211192,17z/data=!4m5!3m4!1s0x0:0xf1c683a298e69e7b!8m2!3d23.8408767!4d91.4214196">Click here</a>').openPopup();
+marker.bindPopup("<b>NIT Agartala</b><br>SANGANAN PRAYOG<br> <a target=\"_blank\" href=\"https://www.google.com/maps/place/National+Institute+of+Technology+Agartala/@23.8406804,91.4211192,17z/data=!4m5!3m4!1s0x0:0xf1c683a298e69e7b!8m2!3d23.8408767!4d91.4214196\">Click here</a>").openPopup();
 
-// Smooth Scrolling
+/* // Smooth Scrolling
 $('#navbar a, .btn').on('click', function(event) {
     if (this.hash !== '') {
         event.preventDefault();
@@ -25,6 +25,29 @@ $('#navbar a, .btn').on('click', function(event) {
             800
         );
     }
+}); */
+
+// Smooth Scrolling
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("#navbar a").on('click', function(event) {
+
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        }
+    });
 });
 
 // Sticky menu Background
