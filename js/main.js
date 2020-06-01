b721dbb451e080194c9d9df04ed4bfedabeb55f1
Jashaswimalya Acharjee <31704400+jash-maester@users.noreply.github.com>
2020-06-01 12:28:19 +0530

    added more quotes

--- js/main.js
+++ js/main.js
@@ -4,13 +4,17 @@ var typewriter = new Typewriter(app, {
     loop: true
 });
 
-typewriter.typeString('"If you want to shine like a sun, first burn like a sun." <br>- Dr. APJ Abdul Kalam')
+typewriter.typeString('"If you want to shine like a sun, first burn like a sun." <br>- <strong>Dr. APJ Abdul Kalam</strong>')
+    .changeDeleteSpeed(20)
     .pauseFor(2500)
     .deleteAll()
-    .typeString('"Things can get out of a black hole both on the outside and possibly to another Universe. <br>So, If you feel you are in a black hole, don\'t give up - there\'s a way out" <br> - Stephen Hawking')
+    .typeString('"Things can get out of a black hole both on the outside and possibly to another Universe. <br>So, If you feel you are in a black hole, don\'t give up - there\'s a way out" <br> - <strong>Stephen Hawking</strong>')
     .pauseFor(2500)
     .deleteAll()
-    .typeString(' "only two things are infinite, the universe and human stupidity,<br> and I\'m not sure about the former."<br>- Albert Einstein.')
+    .typeString('"The cosmos is within us. We are made of starstuff. We are a way for the universe to know itself." <br>- <strong>Carl Sagan</strong>')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('"The first step is to establish that something is possible, then - probability will occur." <br>- <strong>Elon Musk</strong>')
     .pauseFor(2500)
     .start();
 
@@ -28,21 +32,6 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mymap);
 var marker = L.marker([23.8408767, 91.4214196]).addTo(mymap);
 marker.bindPopup("<b>NIT Agartala</b><br>SANGANAN PRAYOG<br> <a target=\"_blank\" href=\"https://www.google.com/maps/place/National+Institute+of+Technology+Agartala/@23.8406804,91.4211192,17z/data=!4m5!3m4!1s0x0:0xf1c683a298e69e7b!8m2!3d23.8408767!4d91.4214196\">Click here</a>").openPopup();
-
-/* // Smooth Scrolling
-$('#navbar a, .btn').on('click', function(event) {
-    if (this.hash !== '') {
-        event.preventDefault();
-
-        const hash = this.hash;
-
-        $('html, body').animate({
-                scrollTop: $(hash).offset().top - 100
-            },
-            800
-        );
-    }
-}); */
 
 // Smooth Scrolling
 $(document).ready(function() {
